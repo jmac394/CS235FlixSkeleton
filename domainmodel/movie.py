91581e93bd6d69b4c29c2eb85fb54feb53ea2968
jmac394 <jmac394@aucklanduni.ac.nz>
2020-09-04 23:12:34 +1200

    Edited file

--- domainmodel/movie.py
+++ domainmodel/movie.py
@@ -3,4 +3,125 @@ from domainmodel.actor import Actor
 from domainmodel.director import Director
 
 class Movie:
-    pass
+    def __init__(self, movie_name: str, release_year: int):
+        if movie_name == "" or type(movie_name) is not str:
+            self.__movie_name = None
+        else:
+            self.__movie_name = movie_name.strip()
+        if isinstance(release_year, int):
+            if release_year >= 1900:
+                self.__release_year = release_year
+            else:
+                self.__release_year = None
+        else:
+            self.__release_year = None
+        self.__actor_list = []
+        self.__genre_list = []
+        self.__description = None
+        self.__director = None
+        self.__runtime = None
+
+    @property
+    def movie_name(self) -> str:
+        return self.__movie_name
+
+    def __repr__(self):
+        return f"<Movie {self.__movie_name}, {str(self.__release_year)}>"
+
+    def __eq__(self, other):
+        if repr(self) == repr(other):
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if repr(self) < repr(other):
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def add_actor(self, actor):
+        if actor not in self.__actor_list and isinstance(actor, Actor):
+            self.__actor_list.append(actor)
+
+    def remove_actor(self, actor):
+        if actor in self.__actor_list:
+            self.__actor_list.remove(actor)
+
+    def add_genre(self, genre):
+        if genre not in self.__genre_list and isinstance(genre, Genre):
+            self.__genre_list.append(genre)
+
+    def remove_genre(self, genre):
+        if genre in self.__genre_list:
+            self.__genre_list.remove(genre)
+
+    def return_actor(self):
+        return self.__actor_list
+
+    def return_genre(self):
+        return self.__genre_list
+
+    @property
+    def title(self):
+        return self.__movie_name
+
+    @title.setter
+    def title(self, new_name):
+        if isinstance(new_name, str):
+            self.__movie_name = new_name.strip()
+        else:
+            pass
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, new_description):
+        if isinstance(new_description, str):
+            if new_description != "":
+                self.__description = new_description.strip()
+        else:
+            pass
+
+    @property
+    def director(self):
+        return self.__director
+
+    @director.setter
+    def director(self, new_director: Director):
+        self.__director = new_director
+
+    @property
+    def actors(self):
+        return self.__actor_list
+
+    @actors.setter
+    def actors(self, new_actor: Actor):
+        self.__actor_list.append(new_actor)
+
+    @property
+    def genres(self):
+        return self.__genre_list
+
+    @genres.setter
+    def genres(self, new_genre: Genre):
+        self.__genre_list.append(new_genre)
+
+    @property
+    def runtime_minutes(self):
+        return self.__runtime
+
+    @runtime_minutes.setter
+    def runtime_minutes(self, runtime):
+        if isinstance(runtime, int):
+            if runtime > 0:
+                self.__runtime = runtime
+            else:
+                raise ValueError()
+        else:
+            pass
